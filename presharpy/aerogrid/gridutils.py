a9db7b6be8cd2f4e83279a8d48adef45b0042e9d
Alfonso del Carre <fonsocarre@gmail.com>
2016-10-10 17:53:44 +0100

    Added rotation matrix generation from triad

--- presharpy/aerogrid/gridutils.py
+++ presharpy/aerogrid/gridutils.py
@@ -20,27 +20,39 @@ def skew(vector):
     matrix[2, 1] = vector[0]
     return matrix
 
+#
+# def crv_from_vectors(veca, vecb):
+#     # import pdb;pdb.set_trace()
+#     if np.linalg.norm(np.cross(veca,vecb)) < 1e-8:
+#         axis = unit_vector(veca)
+#     else:
+#         axis = np.cross(veca,vecb)/np.linalg.norm(np.cross(veca, vecb))
+#     angle = np.arccos(np.dot(unit_vector(veca), unit_vector(vecb)))
+#     return axis*angle
+#
+#
+# def crv2rot(crv):
+#     rot = np.eye(3)
+#     if np.linalg.norm(crv) < 1e-8:
+#         return rot
+#
+#     crv_norm = np.linalg.norm(crv)
+#     rot += (np.sin(crv_norm)/crv_norm)*skew(crv)
+#     rot += (1 - np.cos(crv_norm))/(crv_norm)*np.dot(skew(crv), skew(crv))
+#     return rot
 
-def crv_from_vectors(veca, vecb):
-    # import pdb;pdb.set_trace()
-    if np.linalg.norm(np.cross(veca,vecb)) < 1e-8:
-        axis = unit_vector(veca)
-    else:
-        axis = np.cross(veca,vecb)/np.linalg.norm(np.cross(veca, vecb))
-    angle = np.arccos(np.dot(unit_vector(veca), unit_vector(vecb)))
-    return axis*angle
 
-
-def crv2rot(crv):
-    rot = np.eye(3)
-    if np.linalg.norm(crv) < 1e-8:
-        return rot
-
-    crv_norm = np.linalg.norm(crv)
-    rot += (np.sin(crv_norm)/crv_norm)*skew(crv)
-    rot += (1 - np.cos(crv_norm))/(crv_norm)*np.dot(skew(crv), skew(crv))
+def triad2rot(xb, yb, zb):
+    '''
+    If the input triad is the "b" coord system given in "a" frame,
+    this function returns Rab
+    :param xb:
+    :param yb:
+    :param zb:
+    :return: rotation matrix Rab
+    '''
+    rot = np.column_stack((xb, yb, zb))
     return rot
-
 
 if __name__ == '__main__':
     pass
