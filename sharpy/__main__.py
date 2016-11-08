125a4b312a71ee4f4eafdfbd1d37978dc8fb2283
Alfonso del Carre <fonsocarre@gmail.com>
2016-11-08 14:27:03 +0000

    More changes in main and input files

--- sharpy/__main__.py
+++ sharpy/__main__.py
@@ -4,15 +4,8 @@ import os
 from presharpy.problemdata import ProblemData
 from sharpy.beam.solver.nonlinearstatic import NonLinearStatic
 import sharpy.utils.solver_interface as solver_interface
+import sharpy.utils.input as input_arg
 
 solver_interface.print_available_solvers()
 
-if len(sys.argv) == 1:
-    print('Running SHARPy using the default case name in the current directory:')
-    print('./case.solver.txt is the main settings file')
-    case_name = 'case'
-    case_route = './'
-elif len(sys.argv) == 2:
-    case_name, case_route = os.path.split(sys.argv[1])
-    print('Running SHARPy using the case: %s' % case_name)
-    print('in the folder: %s' % case_route)
+input_arg.read_settings()
---
+++ sharpy/utils/input.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def read_settings():
+    if len(sys.argv) == 1:
+        print('Running SHARPy using the default settings file:')
+        case_settings = './test.solver.txt'
+        print('%s is the main settings file' % case_settings)
+    elif len(sys.argv) == 2:
+        case_settings = sys.argv[1]
+        print('Running SHARPy using the settings file: %s' % case_settings)
+    else:
+        print('*** Too many arguments, only the first one will be used')
+        case_settings = sys.argv[1]
+        print('Running SHARPy using the settings file: %s' % case_settings)
+
+    settings = parse_settings(case_settings)
+    return settings
+
+
+def parse_settings(file):
+    from presharpy.utils.settings import load_config_file
+    settings = load_config_file(file)
+    return settings
