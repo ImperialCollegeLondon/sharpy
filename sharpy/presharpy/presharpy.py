b58a8a578bb5ea6d9753447697ee5fe9fe88f3dc
Alfonso del Carre <fonsocarre@gmail.com>
2017-10-24 11:21:06 +0100

    presharpy cleaned up

--- sharpy/presharpy/presharpy.py
+++ sharpy/presharpy/presharpy.py
@@ -36,64 +36,8 @@ class PreSharpy(object):
             except KeyError:
                 exceptions.NotImplementedSolver(solver_name)
 
-        # self.initialise()
-
     def initialise(self):
         pass
-        # fem_file_name = self.case_route + '/' + self.case_name + '.fem.h5'
-        # if not self.only_structural:
-        #     aero_file_name = self.case_route + '/' + self.case_name + '.aero.h5'
-        #
-        # if not self.steady_problem:
-        #     dyn_file_name = self.case_route + '/' + self.case_name + '.dyn.h5'
-
-        # Check if files exist
-        # h5utils.check_file_exists(fem_file_name)
-        # print('\tThe FEM file exists, it will be loaded.')
-        # if not self.only_structural:
-        #     h5utils.check_file_exists(aero_file_name)
-        #     print('\tThe aero file exists, it will be loaded.')
-        # if not self.steady_problem:
-        #     h5utils.check_file_exists(dyn_file_name)
-        #     print('\tThe dynamic forces file exists, it will be loaded.')
-
-        # Assign handles
-        # !!!
-        # These handles are stored in the class.
-        # That means that they will stay open until
-        # we manually close them or the instance of
-        # ProblemData is destroyed
-        # self.fem_handle = h5.File(fem_file_name, 'r')
-        # """h5py.File: .fem.h5 file handle"""
-        # if not self.only_structural:
-        #     self.aero_handle = h5.File(aero_file_name, 'r')
-        #     """h5py.File: .aero.h5 file handle"""
-        # if not self.steady_problem:
-        #     self.dyn_handle = h5.File(dyn_file_name, 'r')
-        #
-        # # Store h5 info in dictionaries
-        # self.fem_data_dict = (
-        #     h5utils.load_h5_in_dict(self.fem_handle))
-        # """dict: contains all the input data of the ``FEM`` file stored in a dictionary"""
-        # h5utils.check_fem_dict(self.fem_data_dict)
-        #
-        # if not self.only_structural:
-        #     self.aero_data_dict = (
-        #         h5utils.load_h5_in_dict(self.aero_handle))
-        #     """dict: contains all the input data of the ``aero`` file stored in a dictionary"""
-        #     # h5utils.check_aero_dict(self.aero_data_dict)   #TODO
-        #
-        # if not self.steady_problem:
-        #     self.dyn_data_dict = (
-        #         h5utils.load_h5_in_dict(self.dyn_handle))
-        #
-        # cout.cout_wrap('\tDONE')
-        # cout.cout_wrap(cout.separator)
-        # cout.cout_wrap('Processing fem input and generating beam model...')
-        # if not self.steady_problem:
-        #     self.beam = beam.Beam(self.fem_data_dict, self.dyn_data_dict)
-        # else:
-        #     self.beam = beam.Beam(self.fem_data_dict)
 
     @staticmethod
     def load_config_file(file_name):
