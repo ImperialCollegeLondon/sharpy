5cbae96327657af44b6d563c4648520696152cb2
Alfonso del Carre <fonsocarre@gmail.com>
2017-10-26 14:35:17 +0100

    Generator interface and autoloading

--- sharpy/generators/__init__.py
+++ sharpy/generators/__init__.py
@@ -1,15 +1,16 @@
 import importlib
 import os
 
-import sharpy.utils.solver_interface as solver_interface
+import sharpy.utils.generator_interface as generator_interface
 import sharpy.utils.sharpydir as sharpydir
 
-files = solver_interface.solver_list_from_path(os.path.dirname(__file__))
+files = generator_interface.generator_list_from_path(os.path.dirname(__file__))
 
 import_path = os.path.dirname(__file__)
 import_path = import_path.replace(sharpydir.SharpyDir, "")
-if import_path[0] == "/": import_path = import_path[1:]
+if import_path[0] == "/":
+    import_path = import_path[1:]
 import_path = import_path.replace("/", ".")
 
 for file in files:
-    solver_interface.solvers[file] = importlib.import_module(import_path + "." + file)
+    generator_interface.generators[file] = importlib.import_module(import_path + "." + file)
