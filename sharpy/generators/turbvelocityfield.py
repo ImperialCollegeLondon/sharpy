6768d6955d40b4cf26cefa75b72d3958c84c46a8
Alfonso del Carre <fonsocarre@gmail.com>
2019-02-08 14:30:23 +0000

    Start of time domain interpolator

--- sharpy/generators/turbvelocityfield.py
+++ sharpy/generators/turbvelocityfield.py
@@ -3,6 +3,7 @@ import scipy.interpolate as interpolate
 import h5py as h5
 import os
 from xml.dom import minidom
+from lxml import objectify
 
 import sharpy.utils.generator_interface as generator_interface
 import sharpy.utils.settings as settings
@@ -11,6 +12,48 @@ import sharpy.utils.cout_utils as cout
 
 @generator_interface.generator
 class TurbVelocityField(generator_interface.BaseGenerator):
+    r"""
+    Turbulent Velocity Field Generator
+
+    ``TurbVelocitityField`` is a class inherited from ``BaseGenerator``
+
+    The ``TurbVelocitityField`` class generates a velocity field based on the input from an [XDMF](http://www.xdmf.org) file.
+    It supports time-dependant fields as well as frozen turbulence.
+
+    To call this generator, the ``generator_id = TurbVelocityField`` shall be used.
+    This is parsed as the value for the ``velocity_field_generator`` key in the desired aerodynamic solver's settings.
+
+    Supported files:
+        - `field_id.xdmf`: Steady or Unsteady XDMF file
+
+    This generator also performs time interpolation between two different time steps. For now, only linear interpolation is possible.
+
+    Space interpolation is done through `scipy.interpolate` trilinear interpolation. However, turbulent fields are
+    read directly from the binary file and not copied into memory. This is performed using `np.memmap`.
+    The overhead of this procedure is ~18% for the interpolation stage, however, initially reading the binary velocity field
+    (which will be much more common with time-domain simulations) is faster by a factor of 1e4.
+    Also, memory savings are quite substantial: from 6Gb for a typical field to a handful of megabytes for the whole program.
+
+    Args:
+        in_dict (dict): Input data in the form of dictionary. See acceptable entries below:
+
+            ===================  ===============  ===============================================================  ===================
+            Name                 Type             Description                                                      Default
+            ===================  ===============  ===============================================================  ===================
+            ``print_info``       ``bool``         Output solver-specific information in runtime.                   ``True``
+            ``turbulent_field``  ``str``          XDMF file path of the velocity file.                             ``None``
+            ``offset``           ``list(float)``  Spatial offset in the 3 dimensions                               ``[0.0, 0.0, 0.0]``
+            ``centre_y``         ``bool``         Flag for changing the domain to [-y_max/2, y_max/2].             ``True``
+            ``periodicity``      ``str``          Axes in which periodicity is enforced                            ``xy``
+            ``frozen``           ``bool``         If True, the turbulent field will not be updated in time.        ``True``
+            ===================  ===============  ===============================================================  ===================
+
+    Attributes:
+
+    See Also:
+        .. py:class:: sharpy.utils.generator_interface.BaseGenerator
+
+    """
     generator_id = 'TurbVelocityField'
 
     def __init__(self):
@@ -24,9 +67,6 @@ class TurbVelocityField(generator_interface.BaseGenerator):
         self.settings_types['turbulent_field'] = 'str'
         self.settings_default['turbulent_field'] = None
 
-        self.settings_types['u_inf'] = 'float'
-        self.settings_default['u_inf'] = 0.
-
         self.settings_types['offset'] = 'list(float)'
         self.settings_default['offset'] = np.zeros((3,))
 
@@ -36,21 +76,20 @@ class TurbVelocityField(generator_interface.BaseGenerator):
         self.settings_types['periodicity'] = 'str'
         self.settings_default['periodicity'] = 'xy'
 
+        self.settings_types['frozen'] = 'bool'
+        self.settings_default['frozen'] = True
+
         self.settings = dict()
 
         self.file = None
         self.extension = None
-        self.turb_time = None
-        self.turb_x_initial = None
-        self.turb_y_initial = None
-        self.turb_z_initial = None
-        self.turb_u_ref = None
-        self.turb_data = None
 
         self.bbox = None
         self.interpolator = 3*[None]
         self.x_periodicity = False
         self.y_periodicity = False
+
+        self.grid_data = dict()
 
     def initialise(self, in_dict):
         self.in_dict = in_dict
@@ -75,22 +114,6 @@ class TurbVelocityField(generator_interface.BaseGenerator):
     # these functions need to define the interpolators
     def read_btl(self, in_file):
         raise NotImplementedError('The BTL reader is not up to date!')
-        # load the turbulent field HDF5
-        # with h5.File(self.settings['turbulent_field']) as self.file:
-        #     # make time to increase from -t to 0 instead of 0 to t
-        #     try:
-        #         self.turb_time = self.file['time'].value
-        #         self.turb_time = self.turb_time - np.max(self.turb_time)
-        #         self.turb_u_ref = self.file['u_inf'].value
-        #         self.turb_x_initial = self.turb_time*self.turb_u_ref + self.settings['offset'][0]
-        #     except KeyError:
-        #         self.turb_x_initial = self.file['x_grid'].value - np.max(self.file['x_grid'].value) + self.settings['offset'][0]
-        #     self.turb_y_initial = self.file['y_grid'].value + self.settings['offset'][1]
-        #     self.turb_z_initial = self.file['z_grid'].value + self.settings['offset'][2]
-        #
-        #     self.turb_data = self.h5file['data/velocity'].value
-        #
-        #     self.init_interpolator(self.turb_data, self.turb_x_initial, self.turb_y_initial, self.turb_z_initial)
 
     def read_xdmf(self, in_file):
         # store route of file for the other files
@@ -101,50 +124,45 @@ class TurbVelocityField(generator_interface.BaseGenerator):
             data = self.file.read().replace('\n', '')
 
         # parse data
-        from lxml import objectify
         tree = objectify.fromstring(data)
 
         # mesh dimensions
-        dimensions = np.fromstring(tree.Domain.Topology.attrib['Dimensions'],
-                                   sep=' ',
-                                   count=3,
-                                   dtype=int)
+        self.grid_data['dimensions'] = np.fromstring(tree.Domain.Topology.attrib['Dimensions'],
+                                                     sep=' ',
+                                                     count=3,
+                                                     dtype=int)
 
         # origin
-        # NOTE: we can count here the offset?
-        origin = np.fromstring(tree.Domain.Geometry.DataItem[0].text,
-                               sep=' ',
-                               count=int(tree.Domain.Geometry.DataItem[0].attrib['Dimensions']),
-                               dtype=float)
+        self.grid_data['origin'] = np.fromstring(tree.Domain.Geometry.DataItem[0].text,
+                                                 sep=' ',
+                                                 count=int(tree.Domain.Geometry.DataItem[0].attrib['Dimensions']),
+                                                 dtype=float)
 
         # dxdydz
         # because of how XDMF does it, it is actually dzdydx
-        dxdydz = np.fromstring(tree.Domain.Geometry.DataItem[1].text,
-                               sep=' ',
-                               count=int(tree.Domain.Geometry.DataItem[1].attrib['Dimensions']),
-                               dtype=float)
+        self.grid_data['dxdydz'] = np.fromstring(tree.Domain.Geometry.DataItem[1].text,
+                                                 sep=' ',
+                                                 count=int(tree.Domain.Geometry.DataItem[1].attrib['Dimensions']),
+                                                 dtype=float)
         # now onto the grid
-        n_grid = len(tree.Domain.Grid.Grid)
-        grid = [dict()]*n_grid
+        self.grid_data['n_grid'] = len(tree.Domain.Grid.Grid)
+        self.grid_data['grid'] = [dict()]*self.grid_data['n_grid']
+# TODO make sure this read all the tsteps
         for i, i_grid in enumerate(tree.Domain.Grid.Grid):
             # cycle through attributes
             for k_attrib, v_attrib in i_grid.attrib.items():
-                grid[i][k_attrib] = v_attrib
-
-        if n_grid > 1:
-            cout.cout_wrap('CAREFUL: n_grid > 1, but we don\' support time series yet')
-
-        # get Attributes (upper case A is not a mistake)
-        for i_attrib, attrib in enumerate(i_grid.Attribute):
-            grid[0][attrib.attrib['Name']] = dict()
-            grid[0][attrib.attrib['Name']]['file'] = attrib.DataItem.text.replace(' ', '')
+                self.grid_data['grid'][i][k_attrib] = v_attrib
+            # get Attributes (upper case A is not a mistake)
+            for i_attrib, attrib in enumerate(i_grid.Attribute):
+                self.grid_data['grid'][i_grid][attrib.attrib['Name']] = dict()
+                self.grid_data['grid'][i_grid][attrib.attrib['Name']]['file'] = attrib.DataItem.text.replace(' ', '')
 
         # now we have the file names and the dimensions
-        self.initial_x_grid = np.array(np.arange(0, dimensions[2]))*dxdydz[2]
+        self.grid_data['initial_x_grid'] = np.array(np.arange(0, self.grid_data['dimensions'][2]))*self.grid_data['dxdydz'][2]
         # z in the file is -y for us in sharpy (y_sharpy = right)
-        self.initial_y_grid = -np.array(np.arange(0, dimensions[0]))*dxdydz[0]
+        self.initial_y_grid = -np.array(np.arange(0, self.grid_data['dimensions'][0]))*self.grid_data['dxdydz'][0]
         # y in the file is z for us in sharpy (up)
-        self.initial_z_grid = np.array(np.arange(0, dimensions[1]))*dxdydz[1]
+        self.initial_z_grid = np.array(np.arange(0, self.grid_data['dimensions'][1]))*self.grid_data['dxdydz'][1]
 
         # the domain now goes:
         # x \in [0, dimensions[0]*dx]
@@ -169,6 +187,12 @@ class TurbVelocityField(generator_interface.BaseGenerator):
             cout.cout_wrap(' y = [' + str(self.bbox[1, 0]) + ', ' + str(self.bbox[1, 1]) + ']', 1)
             cout.cout_wrap(' z = [' + str(self.bbox[2, 0]) + ', ' + str(self.bbox[2, 1]) + ']', 1)
 
+    def read_grid(i_grid):
+        if n_grid > 1:
+            cout.cout_wrap('CAREFUL: n_grid > 1, but we don\'t support time series yet')
+
+
+
         # now we load the velocities (one by one, so we don't store all the
         # info more than once at the same time)
         velocities = ['ux', 'uz', 'uy']
@@ -183,11 +207,11 @@ class TurbVelocityField(generator_interface.BaseGenerator):
             vel = np.swapaxes(vel.reshape((dimensions[2], dimensions[1], dimensions[0]),
                                           order='F')*velocities_mult[i_dim], 1, 2)
 
-            self.init_interpolator(vel,
-                                   self.initial_x_grid,
-                                   self.initial_y_grid,
-                                   self.initial_z_grid,
-                                   i_dim=i_dim)
+            self.interpolator[i_dim] = self.init_interpolator(vel,
+                                                              self.initial_x_grid,
+                                                              self.initial_y_grid,
+                                                              self.initial_z_grid,
+                                                              i_dim=i_dim)
 
     def generate(self, params, uext):
         zeta = params['zeta']
@@ -208,16 +232,18 @@ class TurbVelocityField(generator_interface.BaseGenerator):
 
     def init_interpolator(self, data, x_grid, y_grid, z_grid, i_dim=None):
         if i_dim is None:
+            interpolator = list()
             for i_dim in range(3):
-                self.interpolator[i_dim] = interpolate.RegularGridInterpolator((z_grid, y_grid, x_grid),
-                                                                               data[i_dim, :, :, :],
-                                                                               bounds_error=False,
-                                                                               fill_value=0.0)
+                interpolator.append(interpolate.RegularGridInterpolator((z_grid, y_grid, x_grid),
+                                                                        data[i_dim, :, :, :],
+                                                                        bounds_error=False,
+                                                                        fill_value=0.0))
         else:
-            self.interpolator[i_dim] = interpolate.RegularGridInterpolator((x_grid, y_grid, z_grid),
-                                                                           data,
-                                                                           bounds_error=False,
-                                                                           fill_value=0.0)
+            interpolator = interpolate.RegularGridInterpolator((x_grid, y_grid, z_grid),
+                                                               data,
+                                                               bounds_error=False,
+                                                               fill_value=0.0)
+        return interpolator
 
     def interpolate_zeta(self, zeta, for_pos, u_ext):
         for i_dim in range(3):
