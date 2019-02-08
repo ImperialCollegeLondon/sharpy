015d389fdd95395a0f4c6b11ee866c0e8909ed9d
Alfonso del Carre <fonsocarre@gmail.com>
2019-02-08 16:10:50 +0000

    Corrected bug in sharpy_vars that assumed MKL location

--- bin/sharpy_vars.sh
+++ bin/sharpy_vars.sh
@@ -5,13 +5,6 @@
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$SCRIPTPATH:$PATH
 echo "SHARPy added to PATH from the directory: "$SCRIPTPATH
-#DIRECTORY=$(cd `dirname $0` && pwd)
-#echo $DIRECTORY
-#export PATH=$DIRECTORY:$PATH
-# export OMP_NUM_THREADS=8
-source /opt/intel/mkl/bin/mklvars.sh intel64
 
 SCRIPTPATH=$SCRIPTPATH"/.."
 export PYTHONPATH=$SCRIPTPATH:$PYTHONPATH
-#DIRECTORY=$DIRECTORY"/.."
-#export PYTHONPATH=$DIRECTORY:$PYTHONPATH
