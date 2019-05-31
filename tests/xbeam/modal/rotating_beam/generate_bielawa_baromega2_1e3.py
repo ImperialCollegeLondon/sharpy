08e810db0720ecc308f91ecba4c77b57f42a33a1
Alfonso del Carre <fonsocarre@gmail.com>
2019-05-31 15:52:58 +0100

    Updated test cases

--- tests/xbeam/modal/rotating_beam/generate_bielawa_baromega2_1e3.py
+++ tests/xbeam/modal/rotating_beam/generate_bielawa_baromega2_1e3.py
@@ -57,7 +57,7 @@ def generate_fem_file(route, case_name, num_elem, num_node_elem=3):
     y = (np.linspace(0, length, num_node))*np.sin(angle)
     z = np.zeros((num_node,))
 
-    structural_twist = np.zeros_like(x)
+    structural_twist = np.zeros((num_elem, num_node_elem))
 
     frame_of_reference_delta = np.zeros((num_elem, num_node_elem, 3))
     for ielem in range(num_elem):
