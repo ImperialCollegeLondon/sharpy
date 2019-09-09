80c550ca3a2c05e5dfc38912677a3de8031fb123
Alfonso del Carre <alfonso.del-carre14@imperial.ac.uk>
2019-09-09 17:09:34 +0100

    Added documentation to stepuvlm

--- sharpy/solvers/stepuvlm.py
+++ sharpy/solvers/stepuvlm.py
@@ -13,46 +13,69 @@ import sharpy.utils.cout_utils as cout
 
 @solver
 class StepUvlm(BaseSolver):
+    """
+    StepUVLM is the main solver to use for unsteady aerodynamics.
+    """
     solver_id = 'StepUvlm'
+    solver_classification = 'Aerodynamic'
 
     settings_types = dict()
     settings_default = dict()
+    settings_description = dict()
 
     settings_types['print_info'] = 'bool'
     settings_default['print_info'] = True
+    settings_description['print_info'] = 'Print info to screen'
 
     settings_types['num_cores'] = 'int'
     settings_default['num_cores'] = 0
+    settings_description['num_cores'] = 'Number of cores to use in the VLM lib'
 
     settings_types['n_time_steps'] = 'int'
     settings_default['n_time_steps'] = 100
+    settings_description['n_time_steps'] = 'Number of time steps to be run'
 
     settings_types['convection_scheme'] = 'int'
     settings_default['convection_scheme'] = 3
+    settings_description['convection_scheme'] = '0 for fixed wake, 2 for convected with background flow and 3 for full
+    force-free wake'
 
     settings_types['dt'] = 'float'
     settings_default['dt'] = 0.1
+    settings_description['dt'] = 'Time step'
 
     settings_types['iterative_solver'] = 'bool'
     settings_default['iterative_solver'] = False
+    settings_description['iterative_solver'] = 'Not in use'
 
     settings_types['iterative_tol'] = 'float'
     settings_default['iterative_tol'] = 1e-4
+    settings_description['iterative_tol'] = 'Not in use'
 
     settings_types['iterative_precond'] = 'bool'
     settings_default['iterative_precond'] = False
+    settings_description['iterative_precond'] = 'Not in use'
 
     settings_types['velocity_field_generator'] = 'str'
     settings_default['velocity_field_generator'] = 'SteadyVelocityField'
+    settings_description['velocity_field_generator'] = 'Name of the velocity field generator to be used in the
+    simulation'
 
     settings_types['velocity_field_input'] = 'dict'
     settings_default['velocity_field_input'] = {}
+    settings_description['velocity_field_input'] = 'Dictionary of settings for the velocity field generator'
 
     settings_types['gamma_dot_filtering'] = 'int'
     settings_default['gamma_dot_filtering'] = 0
+    settings_description['gamma_dot_filtering'] = 'Filtering parameter for the Welch filter for the Gamma_dot
+    estimation. Used when ``unsteady_force_contribution`` is ``on``.'
 
     settings_types['rho'] = 'float'
-    settings_default['rho'] = 1.225
+    settings_default['rho'] = 2.225
+    settings_description['rho'] = 'Air density'
+
+    settings_table = settings.SettingsTable()
+    __doc__ += settings_table.generate(settings_types, settings_default, settings_description)
 
     def __init__(self):
         self.data = None
