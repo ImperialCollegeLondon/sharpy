5b0193a069f3ccf571ea243fd640f76764ac7fe8
Alfonso del Carre <fonsocarre@gmail.com>
2019-11-04 14:35:44 +0000

    Updated documentation of GridBox generator

--- sharpy/generators/gridbox.py
+++ sharpy/generators/gridbox.py
@@ -17,55 +17,43 @@ class GridBox(generator_interface.BaseGenerator):
     """
     generator_id = 'GridBox'
 
+    settings_types = dict()
+    settings_default = dict()
+    settings_description = dict()
+
+    settings_types['coords_0'] = 'list(float)'
+    settings_default['coords_0'] = [0., 0., 0.]
+    settings_description['coords_0'] = 'First bounding box corner'
+
+    settings_types['coords_1'] = 'list(float)'
+    settings_default['coords_1'] = [10., 0., 10.]
+    settings_description['coords_1'] = 'Second bounding box corner'
+
+    settings_types['spacing'] = 'list(float)'
+    settings_default['spacing'] = [1., 1., 1.]
+    settings_description['spacing'] = 'Spacing parameters of the bbox'
+
+    settings_table = settings.SettingsTable()
+    __doc__ += settings_table.generate(settings_types, settings_default, settings_description)
+
     def __init__(self):
         self.in_dict = dict()
-        self.settings_types = dict()
-        self.settings_default = dict()
-
-        self.settings_types['x0'] = 'float'
-        self.settings_default['x0'] = 0.
-
-        self.settings_types['y0'] = 'float'
-        self.settings_default['y0'] = 0.
-
-        self.settings_types['z0'] = 'float'
-        self.settings_default['z0'] = 0.
-
-        self.settings_types['x1'] = 'float'
-        self.settings_default['x1'] = 10.
-
-        self.settings_types['y1'] = 'float'
-        self.settings_default['y1'] = 0.
-
-        self.settings_types['z1'] = 'float'
-        self.settings_default['z1'] = 10.
-
-        self.settings_types['dx'] = 'float'
-        self.settings_default['dx'] = 1.
-
-        self.settings_types['dy'] = 'float'
-        self.settings_default['dy'] = 1.
-
-        self.settings_types['dz'] = 'float'
-        self.settings_default['dz'] = 1.
-
 
     def initialise(self, in_dict):
         self.in_dict = in_dict
         settings.to_custom_types(self.in_dict, self.settings_types, self.settings_default)
 
-        self.x0 = self.in_dict['x0']
-        self.y0 = self.in_dict['y0']
-        self.z0 = self.in_dict['z0']
-        self.x1 = self.in_dict['x1']
-        self.y1 = self.in_dict['y1']
-        self.z1 = self.in_dict['z1']
-        self.dx = self.in_dict['dx']
-        self.dy = self.in_dict['dy']
-        self.dz = self.in_dict['dz']
+        self.x0 = self.in_dict['coords_0'][0]
+        self.y0 = self.in_dict['coords_0'][1]
+        self.z0 = self.in_dict['coords_0'][2]
+        self.x1 = self.in_dict['coords_1'][0]
+        self.y1 = self.in_dict['coords_1'][1]
+        self.z1 = self.in_dict['coords_1'][2]
+        self.dx = self.in_dict['spacing'][0]
+        self.dy = self.in_dict['spacing'][1]
+        self.dz = self.in_dict['spacing'][2]
 
     def generate(self, params):
-
         nx = np.abs(int((self.x1.value-self.x0.value)/self.dx.value + 1))
         ny = np.abs(int((self.y1.value-self.y0.value)/self.dy.value + 1))
         nz = np.abs(int((self.z1.value-self.z0.value)/self.dz.value + 1))
@@ -81,9 +69,6 @@ class GridBox(generator_interface.BaseGenerator):
                     grid[iz][0,ix,iy] = xarray[ix]
                     grid[iz][1,ix,iy] = yarray[iy]
                     grid[iz][2,ix,iy] = zarray[iz]
-                    # grid[ix, iy, iz, 0] = xarray[ix]
-                    # grid[ix, iy, iz, 1] = yarray[iy]
-                    # grid[ix, iy, iz, 2] = zarray[iz]
 
 
         vtk_info = tvtk.RectilinearGrid()
