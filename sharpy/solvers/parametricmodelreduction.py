3671c66ca3f78ab081ee88f4e3359b54cc30c326
Norberto Goizueta <ng213@ic.ac.uk>
2020-02-16 13:49:21 +0000

    Ability to interpolate aero or aeroelastic. Includes other improvements

--- sharpy/solvers/parametricmodelreduction.py
+++ sharpy/solvers/parametricmodelreduction.py
@@ -4,6 +4,8 @@ import sharpy.rom.interpolation.pmorlibrary as pmorlibrary
 import sharpy.rom.utils.librom_interp as librominterp
 import numpy as np
 import itertools
+import sharpy.utils.cout_utils as cout
+from scipy.linalg import block_diag
 
 
 @solver
@@ -35,15 +37,29 @@ class ParametricModelReduction(BaseSolver):
     settings_default = dict()
     settings_options = dict()
 
+    settings_types['print_info'] = 'bool'
+    settings_default['print_info'] = True
+    settings_description['print_info'] = 'Display output to screen'
+
+    settings_types['cases_folder'] = 'str'
+    settings_default['cases_folder'] = ''
+    settings_description['cases_folder'] = 'Path to folder containing cases, a new library will be generated'
+
     settings_types['library_filepath'] = 'str'
     settings_default['library_filepath'] = ''
     settings_description['library_filepath'] = 'Filepath to .pkl file containing pROM library.'
 
     settings_types['reference_case'] = 'int'
     settings_default['reference_case'] = -1
-    settings_description['reference_case'] = 'Reference case for coordinate transformation. If ``-1`` the library' \
-                                             'default value is chosen. If the library has no set default, it will ' \
+    settings_description['reference_case'] = "Reference case for coordinate transformation. If ``-1`` the library's " \
+                                             'default value will be chosen. ' \
+                                             'If the library has no set default, it will ' \
                                              'prompt the user.'
+
+    settings_types['interpolation_system'] = 'str'
+    settings_default['interpolation_system'] = None
+    settings_description['interpolation_system'] = 'System on which to perform the interpolation'
+    settings_options['interpolation_system'] = ['aeroelastic', 'uvlm']
 
     settings_types['projection_method'] = 'str'
     settings_default['projection_method'] = None
@@ -72,6 +88,8 @@ class ParametricModelReduction(BaseSolver):
         self.parameter_index = None
         self.inverse_mapping = None
 
+        self.data = None
+
     def initialise(self, data):
 
         self.settings = data.settings[self.solver_id]
@@ -80,27 +98,37 @@ class ParametricModelReduction(BaseSolver):
 
         self.rom_library = pmorlibrary.ROMLibrary()
 
-        if self.settings['library_filepath'] is '':
+        if self.settings['cases_folder'] is not '':
+            # creates library from a folder containing all cases
+            new_library_settings = {'pickle_source_path': self.settings['cases_folder']}
+            self.rom_library.create(settings=new_library_settings)
+
+        elif self.settings['library_filepath'] is '':
             self.rom_library.interface()
+
         else:
             self.rom_library.load_library(path=self.settings['library_filepath'])
 
         if self.settings['reference_case'] != -1 or self.rom_library.reference_case is None:
             self.rom_library.set_reference_case(self.settings['reference_case'])
 
-        # Create pMOR
-        ss_list = [rom.linear.linear_system.uvlm.ss for rom, _ in self.rom_library.library]
-        vv_list = [rom.linear.linear_system.uvlm.rom['Krylov'].V for rom, _ in self.rom_library.library]
-        wwt_list = [rom.linear.linear_system.uvlm.rom['Krylov'].W.T for rom, _ in self.rom_library.library]
+        if self.settings['print_info']:
+            cout.cout_wrap('Current library for ROM interpolation')
+            self.rom_library.display_library()
+
+        # load the actual data pickles from the pointers in the library
+        self.rom_library.load_data_from_library()
+
+        ss_list, vv_list, wwt_list = self.aeroelastic_bases()  # list of ss and reduced order bases
 
         v_ref = vv_list[self.rom_library.reference_case]
         wt_ref = wwt_list[self.rom_library.reference_case]
 
-        # self.pmor = librominterp.InterpROM(ss_list, vv_list, wwt_list, v_ref, wt_ref,
-        #                                    method_proj=self.settings['projection_method'])
+        self.pmor = librominterp.InterpROM(ss_list, vv_list, wwt_list, v_ref, wt_ref,
+                                           method_proj=self.settings['projection_method'])
 
         # Transform onto gen coordinates
-        # self.pmor.project()
+        self.pmor.project()
 
         # Generate mappings for easier interpolation
         self.sort_grid()
@@ -108,69 +136,84 @@ class ParametricModelReduction(BaseSolver):
         # Future: save for online use?
 
     def run(self):
-        pass
         # keep this section for the online part i.e. the interpolation
-
+        cout.cout_wrap('Running Interpolation')
         # input is list of dicts with parameters at each point
-        input_list = [{'payload': 10, 'u_inf': 30}]  # simulated input
+        input_list = [
+            # {'payload': 10, 'u_inf': 20},
+            {'payload': 20, 'u_inf': 30}
+        ]  # simulated input
 
         for case in input_list:
-            weights = [0] * len(self.rom_library.library)
-            # find lower limits
-            lower_limit = [np.searchsorted(self.param_values[self.parameter_index[parameter]],
-                                           case[parameter], side='right') - 1 for parameter in self.parameters]
-            upper_limit = [np.searchsorted(self.param_values[self.parameter_index[parameter]],
-                                           case[parameter],
-                                           side='left') for parameter in self.parameters]
-            # upper_limit = [current_param + 1 for current_param in lower_limit]
+            weights = self.interpolate_n_sys(case)
+            cout.cout_wrap('Finished interpolating')
 
-            interpolation_weights = np.empty([len(self.parameters)] * len(self.parameters), dtype=float)
-            bin_table = itertools.product(*[[0, 1] for i in range(len(self.parameters))])
-            for permutation in bin_table:
-                print(permutation)
-                current_interpolation_weight = []
-                for parameter in self.parameters:
-                    param_index = self.parameter_index[parameter]
-                    print(parameter)
-                    print(case[parameter])
-                    x_min = self.param_values[param_index][lower_limit[param_index]]
-                    x_max = self.param_values[param_index][upper_limit[param_index]]
-                    print(x_min)
-                    print(x_max)
+            cout.cout_wrap(str(weights))
 
-                    try:
-                        alpha = 1 - (case[parameter] - x_min) / (x_max - x_min)
-                        beta = 1 - alpha
-                    except ZeroDivisionError:
-                        alpha = 1
-                        beta = 0
-                    if permutation[param_index] == 0:
-                        current_interpolation_weight.append(alpha)
-                    elif permutation[param_index] == 1:
-                        current_interpolation_weight.append(beta)
-                    else:
-                        raise ValueError('Permutation incorrect')
+            new_ss = self.pmor(weights)
 
-                interpolation_weights[permutation] = np.prod(current_interpolation_weight)  # visualising corners of the interpolation
+            # call interpolation
 
-                actual_rom_index = [sum(x) for x in zip(permutation, lower_limit)]  # sum of permutation + lower index
+
+        return self.data
+
+    def interpolate_n_sys(self, case):
+        """
+        n-dimensional linear interpolation based on binary operations
+        """
+        weights = [0] * len(self.rom_library.data_library)
+        # find lower limits
+        lower_limit = [np.searchsorted(self.param_values[self.parameter_index[parameter]],
+                                       case[parameter], side='right') - 1 for parameter in self.parameters]
+        upper_limit = [np.searchsorted(self.param_values[self.parameter_index[parameter]],
+                                       case[parameter],
+                                       side='left') for parameter in self.parameters]
+
+        bin_table = itertools.product(*[[0, 1] for i in range(len(self.parameters))])
+        for permutation in bin_table:
+            print(permutation)
+            current_interpolation_weight = []
+            for parameter in self.parameters:
+                param_index = self.parameter_index[parameter]
+                print(parameter)
+                print(case[parameter])
+                x_min = self.param_values[param_index][lower_limit[param_index]]
+                x_max = self.param_values[param_index][upper_limit[param_index]]
+                print(x_min)
+                print(x_max)
+
                 try:
-                    real_rom_index = self.inverse_mapping[tuple(actual_rom_index)]
-                    weights[real_rom_index] = np.prod(current_interpolation_weight)
-                except IndexError:
-                    pass  # no data available hence we don't interpolate
+                    alpha = 1 - (case[parameter] - x_min) / (x_max - x_min)
+                    beta = 1 - alpha
+                except ZeroDivisionError:
+                    alpha = 1
+                    beta = 0
+                if permutation[param_index] == 0:
+                    current_interpolation_weight.append(alpha)
+                elif permutation[param_index] == 1:
+                    current_interpolation_weight.append(beta)
+                else:
+                    raise ValueError('Permutation incorrect')
 
-                if sum(weights) == 1:
-                    if 1 in weights:
-                        print('Hit data point')
-                    break  # you have hit a data point
+            actual_rom_index = [sum(x) for x in zip(permutation, lower_limit)]  # sum of permutation + lower index
+            try:
+                real_rom_index = self.inverse_mapping[tuple(actual_rom_index)]
+                weights[real_rom_index] = np.prod(current_interpolation_weight)
+            except IndexError:
+                pass  # no data available hence we don't interpolate
 
-            print(weights)
+            if sum(weights) == 1:
+                if 1 in weights:
+                    print('Hit data point')
+                break  # you have hit a data point
 
+        if sum(weights) < 1:
+            cout.cout_wrap('Extrapolating data - You are at the edge of your data set', 4)
+        return weights
 
     def sort_grid(self):
 
-        param_library = [parameter for _, parameter in self.rom_library.library]  # list of dicts
+        param_library = [case['parameters'] for case in self.rom_library.library]
         parameters = list(param_library[0].keys())  # all should have the same parameters
         param_values = [[case[param] for case in param_library] for param in parameters]
 
@@ -183,7 +226,7 @@ class ParametricModelReduction(BaseSolver):
         inverse_mapping = np.empty([len(parameter_values[n]) for n in range(len(parameters))], dtype=int)
         mapping = []
         i_case = 0
-        for _, case_parameters in self.rom_library.library:
+        for case_parameters in param_library:
             current_case_mapping = []
             for ith, parameter in enumerate(parameters):
                 p_index = parameter_values[ith].index(case_parameters[parameter])
@@ -198,6 +241,29 @@ class ParametricModelReduction(BaseSolver):
         self.parameter_index = parameter_index
         self.inverse_mapping = inverse_mapping
 
+    def aeroelastic_bases(self):
+
+        if self.settings['interpolation_system'] == 'uvlm':
+            ss_list = [rom.linear.linear_system.uvlm.ss for rom in self.rom_library.data_library]
+            vv_list = [rom.linear.linear_system.uvlm.rom['Krylov'].V for rom in self.rom_library.data_library]
+            wwt_list = [rom.linear.linear_system.uvlm.rom['Krylov'].W.T for rom in self.rom_library.data_library]
+        elif self.settings['interpolation_system'] == 'aeroelastic':
+            ss_list = []
+            vv_list = []
+            wwt_list = []
+            for rom in self.rom_library.data_library:
+                vv = block_diag(rom.linear.linear_system.uvlm.rom['Krylov'].V,
+                                np.eye(rom.linear.linear_system.beam.ss.states))
+                wwt = block_diag(rom.linear.linear_system.uvlm.rom['Krylov'].W.T,
+                                np.eye(rom.linear.linear_system.beam.ss.states))
+                ss_list.append(rom.linear.ss)
+                vv_list.append(vv)
+                wwt_list.append(wwt)
+        else:
+            raise NameError('Unrecognised system on which to perform interpolation')
+
+        return ss_list, vv_list, wwt_list
+
 # def find_nearest(array,value):
 #     idx = np.searchsorted(array, value, side="left")
 #     if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
@@ -206,6 +272,9 @@ class ParametricModelReduction(BaseSolver):
 #         return array[idx]
 
 def f7(seq):
+    """
+    Adds single occurances of an item in a list
+    """
     seen = set()
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
