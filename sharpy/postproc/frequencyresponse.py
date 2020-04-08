e61afbc87f6cf3381cbe9578c683d5c36c8badc4
Norberto Goizueta <ng213@ic.ac.uk>
2020-04-08 09:23:31 +0200

    Remove from FrequencyResponse legacy ROM comparison tools
    Removing some methods from FrequencyResponse that required a ROM to have been calculated or a ROM in a very specific data structure. The idea is to have ROM comparison tools in a different module and use this postproc solely to calculate the frequency response of a linear system.

--- sharpy/postproc/frequencyresponse.py
+++ sharpy/postproc/frequencyresponse.py
@@ -51,16 +51,6 @@ class FrequencyResponse(solver_interface.BaseSolver):
     settings_description['target_system'] = 'System or systems for which to find frequency response.'
     settings_options['target_system'] = ['aeroelastic', 'aerodynamic', 'structural']
 
-    # >>>>>>>>>>>>>>>>>> MOVE TO ROM COMPARISON MODULE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    # settings_types['compute_fom'] = 'bool'
-    # settings_default['compute_fom'] = False
-    # settings_description['compute_fom'] = 'Compute frequency response of full order model (use caution if large).'
-
-    # settings_types['load_fom'] = 'str'
-    # settings_default['load_fom'] = ''
-    # settings_description['load_fom'] = 'Folder to locate full order model frequency response data.'
-    # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
     settings_types['frequency_unit'] = 'str'
     settings_default['frequency_unit'] = 'k'
     settings_description['frequency_unit'] = 'Units of frequency, ``w`` for rad/s, ``k`` reduced.'
@@ -97,16 +87,12 @@ class FrequencyResponse(solver_interface.BaseSolver):
         self.folder = None
         self.print_info = False
 
-        # self.ss = None
-        # self.ssrom = None
-
         self.w_to_k = 1
         self.wv = None
 
     def initialise(self, data, custom_settings=None):
 
         self.data = data
-        # self.ss = data.linear.linear_system.uvlm.ss
 
         if not custom_settings:
             self.settings = self.data.settings[self.solver_id]
@@ -201,45 +187,6 @@ class FrequencyResponse(solver_interface.BaseSolver):
             if self.settings['quick_plot']:
                 self.quick_plot(y_freq_fom, subfolder=system_name)
 
-            # >>>>>>>>>>>>>>>> Move to a dedicated ROM metric module
-            # Y_freq_rom = None
-            # compute_fom = False
-
-            # if self.settings['load_fom'] != '':
-            #     if os.path.exists(self.settings['load_fom']):
-            #         try:
-            #             Y_freq_fom = self.load_frequency_data()
-            #         except OSError:
-            #             compute_fom = True
-            #     else:
-            #         compute_fom = True
-            #
-            # if (self.settings['compute_fom'] and self.settings['load_fom'] == '') or compute_fom:
-            #     if self.settings['print_info']:
-            #         cout.cout_wrap('Computing frequency response...')
-            #         cout.cout_wrap('Full order system:', 1)
-            #     t0fom = time.time()
-            #     Y_freq_fom = system.freqresp(self.wv)
-            #     tfom = time.time() - t0fom
-            #     self.save_freq_resp(self.wv, Y_freq_fom, 'fom')
-            #     if self.settings['print_info']:
-            #         cout.cout_wrap('\tComputed the frequency response of the full order system in %f s' % tfom, 2)
-            #
-            # if self.ssrom is not None:
-            #     if self.settings['print_info']:
-            #         cout.cout_wrap('Computing frequency response...')
-            #         cout.cout_wrap('Reduced order system:', 1)
-            #     t0rom = time.time()
-            #     Y_freq_rom = self.ssrom.freqresp(self.wv)
-            #     trom = time.time() - t0rom
-            #     if self.settings['print_info']:
-            #         cout.cout_wrap('\tComputed the frequency response of the reduced order system in %f s' % trom, 2)
-            #     self.save_freq_resp(self.wv, Y_freq_rom, 'rom')
-            #
-            #     if Y_freq_fom is not None:
-            #         frequency_error(Y_freq_fom, Y_freq_rom, self.wv)
-            # <<<<<<<<<<<<<<<<<< Things to do with ROM comparison need to go elsewhere
-
         return self.data
 
     def find_target_system(self, target_system):
@@ -251,7 +198,7 @@ class FrequencyResponse(solver_interface.BaseSolver):
             ss = self.data.linear.linear_system.beam.ss
 
         elif target_system == 'aerodynamic':
-            ss = self.data.linear.linear_system.uvlm.ss  # this could be a ROM
+            ss = self.data.linear.linear_system.uvlm.ss  # this will be a ROM if a ROM has been calculated
 
         else:
             raise NameError('Unrecognised system')
@@ -331,20 +278,3 @@ class FrequencyResponse(solver_interface.BaseSolver):
             cout.cout_wrap('\tPlots saved to %s' % out_folder, 1)
         except ModuleNotFoundError:
             warnings.warn('Matplotlib not found - skipping plot')
-
-    # >>>>>> Needs to go with ROM >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    # def load_frequency_data(self):
-    #     # TODO: need to change so that it doesn't require self.ss
-    #     if self.settings['print_info']:
-    #         cout.cout_wrap('Loading frequency response from:')
-    #         cout.cout_wrap('\t%s' % self.settings['load_fom'], 1)
-    #     Y_freq_fom = np.zeros((self.ss.outputs, self.ss.inputs, len(self.wv)), dtype=complex)
-    #     for m in range(self.ss.inputs):
-    #         for p in range(self.ss.outputs):
-    #             y_load = np.loadtxt(self.settings['load_fom'] +
-    #                                 '/Y_freq_fom_m%02g_p%02g.dat' %(m,p)).view(complex)
-    #             y_load.shape = (y_load.shape[0], )
-    #             Y_freq_fom[p, m, :] = y_load
-    #
-    #     return Y_freq_fom
-    # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
