06096243b61d3ec838feb95226a02713c0f9c9c9
Unknown <a.munoz-simon@imperial.ac.uk>
2020-05-18 19:21:48 +0100

    Fix commonmark for sphinx 3.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'nbsphinx',
     'sphinx.ext.autosectionlabel',
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
