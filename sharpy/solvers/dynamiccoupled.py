ec4c39ae0333d58872bd7965f142877ee915f319
Norberto Goizueta <ng213@ic.ac.uk>
2020-09-16 10:25:54 +0100

    Remove caller from postproc call

--- sharpy/solvers/dynamiccoupled.py
+++ sharpy/solvers/dynamiccoupled.py
@@ -287,7 +287,7 @@ class DynamicCoupled(BaseSolver):
             self.postprocessors[postproc] = solver_interface.initialise_solver(
                 postproc)
             self.postprocessors[postproc].initialise(
-                self.data, self.settings['postprocessors_settings'][postproc], caller=self)
+                self.data, self.settings['postprocessors_settings'][postproc])
 
         # initialise controllers
         self.controllers = dict()
