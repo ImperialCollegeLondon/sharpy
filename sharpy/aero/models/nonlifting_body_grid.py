72d062ddefdacb2e180b31e6c080e0e4a39d0ab0
sduess <s.dussler20@imperial.ac.uk>
2020-12-08 16:15:36 +0000

    refactor: Delete unnecessary functions
    - Delete functions that are no longer needed since zeta contains only grid nodes instead of collocation points

--- sharpy/aero/models/nonlifting_body_grid.py
+++ sharpy/aero/models/nonlifting_body_grid.py
@@ -39,23 +39,6 @@ class Nonlifting_body_grid(Grid):
             # Get Zeta and Zeta_dot (Panel node positions in G frame)
             nonlifting_body_tstep.zeta[i_surf], nonlifting_body_tstep.zeta_dot[i_surf] = self.get_zeta_and_zeta_dot(i_surf, structure_tstep)
 
-    def get_triangle_center(self, p1, p2, p3):
-        return (p1+p2+p3)/3
-
-    def get_quadrilateral_center(self, list_points):
-        # based on http://jwilson.coe.uga.edu/EMT668/EMT668.Folders.F97/Patterson/EMT%20669/centroid%20of%20quad/Centroid.html
-        array_triangle_centroids = np.zeros((4,3))
-        counter = 0
-        triangle_combinations = [[0, 1, 2], [0, 2, 3], [0, 1, 3], [1, 2, 3]]
-        for triangle in triangle_combinations: #list(itertools.combinations([0, 1, 2, 3], 3)):
-            array_triangle_centroids[counter] = self.get_triangle_center(
-                list_points[triangle[0]],
-                list_points[triangle[1]],
-                list_points[triangle[2]])
-            counter+=1
-        centroid_quadrilateral = self.find_intersection_points(array_triangle_centroids)
-        return centroid_quadrilateral
-
     def get_zeta_and_zeta_dot(self,i_surf, structure_tstep):
         numb_radial_nodes = self.dimensions[i_surf][0] +1
         matrix_nodes = np.zeros((3, numb_radial_nodes,
@@ -101,39 +84,6 @@ class Nonlifting_body_grid(Grid):
         return matrix_nodes, matrix_nodes_dot
 
 
-    def get_collocation_point_pos(self, i_surf, numb_spanwise_elements, structure_tstep):
-        numb_radial_nodes = self.surface_m[i_surf]+1
-        matrix_nodes = self.get_nodes_position(i_surf, structure_tstep, numb_radial_nodes)
-        counter, i_row = 0, 0
-        matrix_collocation = np.zeros(((numb_spanwise_elements)*(numb_radial_nodes-1),3))
-        for i in range(0, numb_spanwise_elements):
-            if i == 0:
-                for i_rad_node in range(0, numb_radial_nodes-1):
-                    matrix_collocation[i_row] = self.get_triangle_center(
-                        matrix_nodes[0],
-                        matrix_nodes[i_rad_node+1],
-                        matrix_nodes[i_rad_node+2])
-                    i_row += 1
-                counter+=1
-            elif i == numb_spanwise_elements-1:
-                for i_rad_node in range(0, numb_radial_nodes-1):
-                    matrix_collocation[i_row] = self.get_triangle_center(
-                        matrix_nodes[-1],
-                        matrix_nodes[counter],
-                        matrix_nodes[counter+1])
-                    i_row+=1
-                    counter+=1
-            else:
-                for i_rad_node in range(0, numb_radial_nodes-1):
-                    matrix_collocation[i_row] = self.get_quadrilateral_center(
-                        matrix_nodes[[counter, counter +1,
-                                      counter+numb_radial_nodes+1,
-                                      counter+numb_radial_nodes]])
-                    counter+=1
-                    i_row+=1
-                counter+=1
-        return matrix_collocation
-
     def get_elment_and_local_node_id(self, i_surf, i_global_node):
         # get beam elements of surface
         idx_beam_elements_surface = np.where(self.surface_distribution == i_surf)[0]
@@ -144,22 +94,6 @@ class Nonlifting_body_grid(Grid):
                 return i_elem, i_local_node
         raise Exception("The global node %u could not be assigned to any element of surface %u." % (i_global_node, i_surf))
 
-    def find_intersection_points(self, array_points):
-        sol_parameter = fsolve(self.intersection_point_equation,
-                               [0.3, 0.3, 0.0], args = tuple(array_points))
-        return array_points[0]+sol_parameter[0]*(array_points[1]-array_points[0])
-
-    def intersection_point_equation(self,z, *data):
-        """
-        Function to determine intersection point between 4 points for fsolve.
-        Math: A+t*(B-A) -C - u*(D-C) = 0
-        TO-DO: Define function in utils as helper function (algebra??)
-        """
-        point_A, point_B, point_C, point_D = data
-        t = z[0]
-        u = z[1]
-        F = point_A+t*(point_B-point_A) - point_C - u*(point_D-point_C)
-        return F
 
 
 
