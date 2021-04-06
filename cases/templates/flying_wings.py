20a84efcbe1f0591ec3cd0d9aaf23592759c4bd2
Norberto Goizueta <ng213@ic.ac.uk>
2021-04-06 09:46:16 +0100

    Support polar files in flyingwings templates

--- cases/templates/flying_wings.py
+++ cases/templates/flying_wings.py
@@ -78,7 +78,8 @@ class FlyingWing():
                  physical_time=2,
                  route='.',
                  case_name='flying_wing',
-                 RollNodes=False):
+                 RollNodes=False,
+                 polar_file=None):
 
         ### parametrisation
         assert n_surfaces < 3, "use 1 or 2 surfaces only!"
@@ -127,6 +128,10 @@ class FlyingWing():
         self.tip_airfoil_P = 0
         self.tip_airfoil_M = 0
 
+        self.polars = None # list of polar for each airfoil
+        if polar_file is not None:
+            self.load_polar(polar_file)
+
         # Numerics for dynamic simulations
         self.dt_factor = 1
         self.n_tstep = None
@@ -150,6 +155,17 @@ class FlyingWing():
         self.control_surface_type = np.zeros((self.n_control_surfaces), dtype=int)
         self.control_surface_deflection = np.zeros((self.n_control_surfaces,))
         self.control_surface_chord = np.array([M//2], dtype=int)
+        self.control_surface_hinge_coord = np.zeros_like(self.control_surface_type, dtype=float)
+
+    def settings_to_config(self, settings):
+        file_name = self.route + '/' + self.case_name + '.sharpy'
+        config = configobj.ConfigObj()
+        config.filename = file_name
+        for k, v in settings.items():
+            config[k] = v
+        config.write()
+
+        return file_name
 
     def update_mass_stiff(self):
         '''This method can be substituted to produce different wing configs'''
@@ -176,6 +192,19 @@ class FlyingWing():
 
         self.lumped_mass[0] = 5.
         self.lumped_mass_position[0] = np.array([0, 0.25, 0])
+
+    def load_polar(self, file):
+        """
+        Read polar from Airfoil Tools polar txt file
+
+        Args:
+            file (str):
+        """
+        polar_raw_data = np.loadtxt(file, skiprows=12)
+        self.polars = np.column_stack((polar_raw_data[:, 0] * np.pi / 180, # aoa
+                                       polar_raw_data[:, 1], # cl
+                                       polar_raw_data[:, 2], # cd
+                                       polar_raw_data[:, 4])) #cm
 
     def update_derived_params(self):
         ### Derived
@@ -670,6 +699,16 @@ class FlyingWing():
                 a_eff_handle = h5file.create_dataset(
                     'airfoil_efficiency', data=airfoil_efficiency)
 
+            if self.control_surface_hinge_coord is not None:
+                cs_hinge = h5file.create_dataset(
+                    'control_surface_hinge_coord', data=self.control_surface_hinge_coord
+                )
+
+            if self.polars is not None:
+                polars_group = h5file.create_group('polars')
+                for i_airfoil in range(len(self.Airfoils_surf)):
+                    polars_group.create_dataset('{:g}'.format(i_airfoil), data=self.polars)
+
     def generate_fem_file(self):
 
         assert hasattr(self, 'conn_glob'), \
@@ -947,6 +986,7 @@ class GolandControlSurface(Goland):
             self.control_surface_deflection[i] = cs_deflection[i] * np.pi/180
         self.control_surface_chord = M // 2 * np.ones(self.n_control_surfaces, dtype=int)
         self.control_surface_type = np.zeros(self.n_control_surfaces, dtype=int)
+        self.control_surface_hinge_coord = np.zeros_like(self.control_surface_type, dtype=float)
         # other
         self.c_ref = 1.8288
         self.pct_flap = pct_flap
@@ -1050,7 +1090,8 @@ class QuasiInfinite(FlyingWing):
                  n_surfaces=1,
                  route='.',
                  case_name='qsinf',
-                 RollNodes=False):
+                 RollNodes=False,
+                 polar_file=None):
         super().__init__(M=M, N=N,
                          Mstar_fact=Mstar_fact,
                          u_inf=u_inf,
@@ -1065,7 +1106,8 @@ class QuasiInfinite(FlyingWing):
                          n_surfaces=n_surfaces,
                          route=route,
                          case_name=case_name,
-                         RollNodes=RollNodes)
+                         RollNodes=RollNodes,
+                         polar_file=polar_file)
         self.c_ref = main_chord
         self.main_ea = 0.5
         self.main_cg = 0.5
