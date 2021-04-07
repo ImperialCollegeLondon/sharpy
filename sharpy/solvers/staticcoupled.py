608f155a89d7ecbc782af88c905cde0341a8f51f
Norberto Goizueta <ng213@ic.ac.uk>
2021-04-07 12:31:53 +0200

    Include correct forces generator in relevant solvers

--- sharpy/solvers/staticcoupled.py
+++ sharpy/solvers/staticcoupled.py
@@ -62,9 +62,10 @@ class StaticCoupled(BaseSolver):
     settings_description['relaxation_factor'] = 'Relaxation parameter in the FSI iteration. 0 is no relaxation and -> 1 is very relaxed'
 
     settings_types['correct_forces_method'] = 'str'
-    settings_default['correct_forces_method'] = '' # 'efficiency'
-    settings_description['correct_forces_method'] = 'Function used to correct aerodynamic forces. Check :py:mod:`sharpy.utils.correct_forces`'
-    settings_options['correct_forces_method'] = ['efficiency', 'PolarCorrection']
+    settings_default['correct_forces_method'] = ''
+    settings_description['correct_forces_method'] = 'Function used to correct aerodynamic forces. ' \
+                                                    'See :py:mod:`sharpy.generators.polaraeroforces`'
+    settings_options['correct_forces_method'] = ['EfficiencyCorrection', 'PolarCorrection']
 
     settings_types['correct_forces_settings'] = 'dict'
     settings_default['correct_forces_settings'] = {}
@@ -85,7 +86,6 @@ class StaticCoupled(BaseSolver):
         self.residual_table = None
 
         self.correct_forces = False
-        self.correct_forces_function = None
         self.correct_forces_generator = None
 
     def initialise(self, data, input_dict=None):
@@ -117,12 +117,12 @@ class StaticCoupled(BaseSolver):
         # Define the function to correct aerodynamic forces
         if self.settings['correct_forces_method'] is not '':
             self.correct_forces = True
-            # self.correct_forces_function = cf.dict_of_corrections[self.settings['correct_forces_method']]
             self.correct_forces_generator = gen_utils.generator_from_string(self.settings['correct_forces_method'])()
             self.correct_forces_generator.initialise(in_dict=self.settings['correct_forces_settings'],
                                                      aero=self.data.aero,
                                                      structure=self.data.structure,
-                                                     rho=self.settings['aero_solver_settings']['rho'])
+                                                     rho=self.settings['aero_solver_settings']['rho'],
+                                                     vortex_radius=self.settings['aero_solver_settings']['vortex_radius'])
 
     def increase_ts(self):
         self.data.ts += 1
@@ -178,11 +178,10 @@ class StaticCoupled(BaseSolver):
                     #                     self.data.aero.timestep_info[self.data.ts],
                     #                     self.data.structure.timestep_info[self.data.ts],
                     #                     struct_forces,
-                    #                     rho=self.aero_solver.settings['rho'])
+                    #                     rho=self.aero_solver.settings['rho'], correct_lift=True)
                     struct_forces = \
                         self.correct_forces_generator.generate(aero_kstep=self.data.aero.timestep_info[self.data.ts],
-                                                               structural_kstep=
-                                                               self.data.structure.timestep_info[self.data.ts],
+                                                               structural_kstep=self.data.structure.timestep_info[self.data.ts],
                                                                struct_forces=struct_forces)
                 self.data.aero.timestep_info[self.data.ts].aero_steady_forces_beam_dof = struct_forces
                 if not self.settings['relaxation_factor'] == 0.:
