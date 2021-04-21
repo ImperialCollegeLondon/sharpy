bcddf3a073c57a4bcbd9171fe0144583f3b82766
sduess <s.dussler20@imperial.ac.uk>
2021-04-21 18:57:57 +0100

    fix: use proper setting style and usage

--- sharpy/postproc/liftdistribution.py
+++ sharpy/postproc/liftdistribution.py
@@ -11,48 +11,50 @@ import sharpy.utils.algebra as algebra
 
 @solver
 class LiftDistribution(BaseSolver):
-    """
-    Exports lift distribution to txt file
+    """LiftDistribution
+    
+    Calculates and exports the lift distribution on lifting surfaces
+    
     """
     solver_id = 'LiftDistribution'
+    solver_classification = 'post-processor'
 
+    settings_types = dict()
+    settings_default = dict()
+    settings_description = dict()
+
+    settings_types['text_file_name'] = 'str'
+    settings_default['text_file_name'] = 'lift_distribution.csv'
+    settings_description['text_file_name'] = 'Text file name'
+
+    settings_default['coefficients'] = True
+    settings_types['coefficients'] = 'bool'
+    settings_description['coefficients'] = 'Calculate aerodynamic lift coefficients'
+
+    settings_types['q_ref'] = 'float'
+    settings_default['q_ref'] = 1
+    settings_description['q_ref'] = 'Reference dynamic pressure'
+
+    settings_table = settings.SettingsTable()
+    __doc__ += settings_table.generate(settings_types, settings_default, settings_description)
     def __init__(self):
-        self.settings_types = dict()
-        self.settings_default = dict()
-
-        self.settings_types['print_info'] = 'bool'
-        self.settings_default['print_info'] = True
-
-        # TO-DO: implement option for normalization
-        self.settings_types['normalise'] = 'bool'
-        self.settings_default['normalise'] = True
-
-        self.settings_types['folder'] = 'str'
-        self.settings_default['folder'] = './output'
-
         self.settings = None
         self.data = None
-
+        self.folder = None
         self.caller = None
 
     def initialise(self, data, custom_settings=None, caller=None):
         self.data = data
-        if custom_settings is None:
-            self.settings = data.settings[self.solver_id]
-        else:
-            self.settings = custom_settings
+        self.settings = data.settings[self.solver_id]
         settings.to_custom_types(self.settings, self.settings_types, self.settings_default)
         self.caller = caller
+        self.folder = data.output_folder
+        if not os.path.exists(self.folder):
+            os.makedirs(self.folder)
 
     def run(self, online=False):
-        if not online:
-            for it in range(len(self.data.structure.timestep_info)):
-                self.lift_distribution(self.data.structure.timestep_info[it],
-                                       self.data.aero.timestep_info[it])
-            cout.cout_wrap('...Finished', 1)
-        else:
-            self.lift_distribution(self.data.structure.timestep_info[self.data.ts],
-                                   self.data.aero.timestep_info[self.data.ts])
+        self.lift_distribution(self.data.structure.timestep_info[self.data.ts],
+                               self.data.aero.timestep_info[self.data.ts])
         return self.data
 
     def lift_distribution(self, struct_tstep, aero_tstep):
@@ -81,7 +83,7 @@ class LiftDistribution(BaseSolver):
                 lift_distribution[inode,1]=struct_tstep.pos[inode, 1]  #y
                 lift_distribution[inode,0]=struct_tstep.pos[inode, 0]  #x
 
-        if self.settings["normalise"]: 
+        if self.settings["coefficients"]: 
             # get lift coefficient           
             strip_area = self.calculate_panel_area(aero_tstep)  
             # TODO: add nondimensional spanwise column y/s
@@ -101,7 +103,7 @@ class LiftDistribution(BaseSolver):
                     lift_distribution[inode,4] /= len(self.data.aero.struct2aero_mapping[inode])
 
         # Export lift distribution data
-        np.savetxt(self.settings["folder"]+'/lift_distribution.txt', lift_distribution, fmt='%10e,'*(numb_col-1)+'%10e', delimiter = ", ", header= header)
+        np.savetxt(os.path.join(self.folder,self.settings['text_file_name']), lift_distribution, fmt='%10e,'*(numb_col-1)+'%10e', delimiter = ", ", header= header)
     
     def calculate_panel_area(self, aero_tstep):
         # Function to get the panel area (TODO: Better description)
