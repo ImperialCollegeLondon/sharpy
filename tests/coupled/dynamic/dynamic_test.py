38f930e5b65207b0e3834d90d9638cf59d553c18
sduess <s.dussler20@imperial.ac.uk>
2022-02-01 17:24:45 +0000

    add: dynamic hale test for unittest

--- tests/coupled/dynamic/dynamic_test.py
+++ tests/coupled/dynamic/dynamic_test.py
@@ -7,18 +7,54 @@ import sharpy.utils.cout_utils as cout
 
 class TestCoupledDynamic(unittest.TestCase):
     """
-    Placeholder for dynamic coupled tests
+    Tests for dynamic coupled problems to identify errors in the unsteady solvers.
+    Implemented tests:
+    - Gust response of the hale aircraft
     """
+
+
 
     @classmethod
     def setUpClass(cls):
         # run all the cases generators
+        case = 'hale'
+        mod = importlib.import_module('tests.coupled.dynamic.' + case + '.generate_' + case)
         pass
 
     @classmethod
     def tearDownClass(cls):
         pass
+    def test_hale_dynamic(self):
+        """
+        Case and results from:
+        tests/coupled/dynamic/hale 
+        reference results produced with SHARPy version 1.3
+        :return:
+        """
+        import sharpy.sharpy_main
 
-    def test_coupled_dynamic1(self):
-        cout.cout_wrap('No tests for dynamic coupled simulations (yet)!', 1)
-        pass
+        case_name = 'hale'
+        route_test_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+        cases_folder = os.path.join(route_test_dir, case_name)
+        output_folder = cases_folder + '/output/'
+
+        sharpy.sharpy_main.main(['', cases_folder + '/hale.sharpy'])
+        n_tstep = 20
+
+        # compare results with reference values 
+        ref_Fz = 50.4986064826483
+        ref_My = -1833.91402522644
+        file = os.path.join(output_folder, case_name, 'beam/beam_loads_%i.csv' %(n_tstep))
+        beam_loads_ts = np.loadtxt(file, delimiter = ',')
+        np.testing.assert_almost_equal(float(beam_loads_ts[0, 6]), ref_Fz,
+                                        decimal=3,
+                                        err_msg='Vertical load on wing root not within 3 decimal points of reference.',
+                                        verbose=True)
+        np.testing.assert_almost_equal(float(beam_loads_ts[0, 8]), ref_My,
+                                        decimal=3,
+                                        err_msg='Pitching moment on wing root not within 3 decimal points of reference.',
+                                        verbose=True)
+
+
+
+
