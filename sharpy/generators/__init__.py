d102d1e4ff9da1967ee7a564dcb031b8a1d0ebee
Alvaro Cea <alvaro_cea@outlook.com>
2022-11-03 12:29:20 +0000

    fix paths to not load symbolic links and consistency with sharpydir

--- sharpy/generators/__init__.py
+++ sharpy/generators/__init__.py
@@ -14,7 +14,7 @@ import sharpy.utils.sharpydir as sharpydir
 
 files = generator_interface.generator_list_from_path(os.path.dirname(__file__))
 
-import_path = os.path.dirname(__file__)
+import_path = os.path.realpath(os.path.dirname(__file__))
 import_path = import_path.replace(sharpydir.SharpyDir, "")
 if import_path[0] == "/":
     import_path = import_path[1:]
