22181d1cdeef118053804920e91b2acd118802dd
Stefanie Düssler <s.dussler20@imperial.ac.uk>
2023-06-15 12:27:58 +0800

    add [tutorial] notebook tutorial for UDP control capabilities of SHARPy

---
+++ docs/source/content/example_notebooks/UDP_control/get_settings_udp.py
@@ -0,0 +1,207 @@
+"""
+    Script to set the required SHARPy settings for  all cases within
+    the UDP Control example notebook. 
+"""
+def get_settings_udp(model, flow, **kwargs):
+    gust = kwargs.get('gust', False)
+    
+    horseshoe = kwargs.get('horseshoe', False)
+    gravity = kwargs.get('gravity', True)
+    wake_length_factor = kwargs.get('wake_length_factor', 10)
+
+    num_cores = kwargs.get('num_cores', 2)
+    num_modes = kwargs.get('num_modes', 20)
+    free_flight = kwargs.get('free_flight', False)
+    tolerance = kwargs.get('tolerance', 1e-6)
+    n_load_steps = kwargs.get('n_load_steps', 5)
+    fsi_tolerance = kwargs.get('fsi_tolerance', 1e-6)
+    relaxation_factor = kwargs.get('relaxation_factor', 0.1)
+    newmark_damp = kwargs.get('newmark_damp', 0.5e-4)
+    output_folder = kwargs.get('output_folder', './output/')
+    model.config['SHARPy'] = {'case': model.case_name,
+                        'route': model.route,
+                        'flow': flow,
+                        'write_screen': 'on',
+                        'write_log': 'on',
+                        'save_settings': 'on',
+                        'log_folder':  output_folder + '/',
+                        'log_file':  model.case_name + '.log'}
+
+
+    model.config['BeamLoader'] = {'unsteady': 'off',
+                                'orientation': model.quat}
+
+    model.config['AerogridLoader'] = {'unsteady': 'on',
+                                'aligned_grid': 'on',
+                                'mstar': wake_length_factor*model.M, #int(20/tstep_factor),
+                                'wake_shape_generator': 'StraightWake',
+                                'wake_shape_generator_input': {
+                                    'u_inf':model.u_inf,
+                                    'u_inf_direction': [1., 0., 0.],
+                                    'dt':model.dt,
+                                },
+                            }
+    if horseshoe:
+        model.config['AerogridLoader']['mstar'] = 1
+
+    model.config['StaticCoupled'] = {
+        'print_info': 'on',
+        'max_iter': 200,
+        'n_load_steps': n_load_steps,
+        'tolerance': 1e-5,
+        'relaxation_factor': relaxation_factor,
+        'aero_solver': 'StaticUvlm',
+        'aero_solver_settings': {
+            'rho': model.rho,
+            'print_info': 'off',
+            'horseshoe': 'on',
+            'num_cores': num_cores,
+            'n_rollup': 0,
+            'velocity_field_generator': 'SteadyVelocityField',
+            'velocity_field_input': {
+                'u_inf': model.u_inf,
+                'u_inf_direction': model.u_inf_direction},
+            'vortex_radius': 1e-9},
+        'structural_solver': 'NonLinearStatic',
+        'structural_solver_settings': {'print_info': 'off',
+                                   'max_iterations': 200,
+                                   'num_load_steps': 5,
+                                   'delta_curved': 1e-6,
+                                   'min_delta': 1e-8,
+                                   'gravity': gravity,
+                                   'gravity': 9.81},
+    }
+
+
+    model.config['AerogridPlot'] = {'include_rbm': 'off',
+                                 'include_applied_forces': 'on',
+                                 'minus_m_star': 0}
+    model.config['BeamPlot'] = {'include_rbm': 'off',
+                             'include_applied_forces': 'on'}
+    
+    model.config['WriteVariablesTime'] = {'structure_variables': ['pos'],
+                                        'structure_nodes': list(range(0, model.num_node_surf)),
+                                        'cleanup_old_solution': 'on',
+                                        }
+    
+    
+    
+    model.config['DynamicCoupled'] = {'print_info': 'on',
+                                  'structural_substeps': 10,
+                                  'dynamic_relaxation': 'on',
+                                  'cleanup_previous_solution': 'on',
+                                  'structural_solver': 'NonLinearDynamicPrescribedStep',
+                                  'structural_solver_settings':  {'print_info': 'off',
+                                                                'max_iterations': 950,
+                                                                'delta_curved': 1e-1,
+                                                                'min_delta': tolerance,
+                                                                'newmark_damp': newmark_damp,
+                                                                'gravity': gravity,
+                                                                'gravity': 9.81,
+                                                                'num_steps': model.n_tstep,
+                                                                'dt':model.dt,
+                                                                },
+                                  'aero_solver': 'StepUvlm',
+                                  'aero_solver_settings':  {'print_info': 'on',
+                                                            'num_cores': num_cores,
+                                                            'convection_scheme': 2,
+                                                            'velocity_field_generator': 'SteadyVelocityField',
+                                                            'velocity_field_input': {'u_inf': model.u_inf,
+                                                                                    'u_inf_direction': [1., 0., 0.]},
+                                                            'rho': model.rho,
+                                                            'n_time_steps': model.n_tstep,
+                                                            'vortex_radius': 1e-9,
+                                                            'dt': model.dt,
+                                                            'gamma_dot_filtering': 3},
+                                  'fsi_substeps': 200,
+                                  'fsi_tolerance': fsi_tolerance,
+                                  'relaxation_factor': model.relaxation_factor,
+                                  'minimum_steps': 1,
+                                  'relaxation_steps': 150,
+                                  'final_relaxation_factor': 0.0,
+                                  'n_time_steps': model.n_tstep,
+                                  'dt': model.dt,
+                                  'include_unsteady_force_contribution': kwargs.get('unsteady_force_distribution', True),
+                                  'postprocessors': ['WriteVariablesTime', 'BeamPlot', 'AerogridPlot'],
+                                  'postprocessors_settings': {'BeamPlot': {'include_rbm': 'on',
+                                                                           'include_applied_forces': 'on'},
+                                                              'StallCheck': {},
+                                                              'AerogridPlot': {
+                                                                  'u_inf': model.u_inf,
+                                                                  'include_rbm': 'on',
+                                                                  'include_applied_forces': 'on',
+                                                                  'minus_m_star': 0},
+                                                              'WriteVariablesTime': {
+                                                                  'structure_variables': ['pos', 'psi'],
+                                                                  'structure_nodes': [model.num_node_surf - 1,
+                                                                                      model.num_node_surf,
+                                                                                      model.num_node_surf + 1],
+                                                                    },
+                                                                    },
+                                    'network_settings': kwargs.get('network_settings', {}),
+                                    }
+    
+    if gust:
+        gust_settings = kwargs.get('gust_settings', {'gust_shape': '1-cos',
+                                                     'gust_length': 10.,
+                                                     'gust_intensity': 0.01,
+                                                     'gust_offset': 0.})
+        model.config['DynamicCoupled']['aero_solver_settings']['velocity_field_generator'] = 'GustVelocityField'
+        model.config['DynamicCoupled']['aero_solver_settings']['velocity_field_input'] =  {'u_inf': model.u_inf,
+                                                                                            'u_inf_direction': [1., 0, 0],
+                                                                                            'relative_motion': bool(not free_flight),
+                                                                                            'offset': gust_settings['gust_offset'],
+                                                                                            'gust_shape': gust_settings['gust_shape'],
+                                                                                            'gust_parameters': {
+                                                                                                                'gust_length': gust_settings['gust_length'],
+                                                                                                                'gust_intensity': gust_settings['gust_intensity'] * model.u_inf,
+                                                                                                            }                                                                
+                                                                                        }
+
+    model.config['PickleData'] = {}
+    
+    model.config['LinearAssembler'] = {'linear_system': 'LinearAeroelastic',
+                                    'inout_coordinates': 'nodes', 
+                                    # 'recover_accelerations': True,           
+                                'linear_system_settings': {
+                                    'beam_settings': {'modal_projection': True,
+                                                        'inout_coords': 'modes',
+                                                        'discrete_time': True,
+                                                        'newmark_damp': newmark_damp,
+                                                        'discr_method': 'newmark',
+                                                        'dt': model.dt,
+                                                        'proj_modes': 'undamped',
+                                                        'num_modes': num_modes,
+                                                        'print_info': 'on',
+                                                        'gravity': gravity,
+                                                        'remove_dofs': []},
+                                    'aero_settings': {'dt': model.dt,
+                                                        'integr_order': 2,
+                                                        'density': model.rho,
+                                                        'remove_predictor': True,
+                                                        'use_sparse': 'off',
+                                                        'gust_assembler':  'LeadingEdge',
+                                                        'ScalingDict': kwargs.get('scaling_dict', {'length':1, 'speed': 1, 'density': 1}),
+                                                        },
+                                    'track_body': free_flight,
+                                    'use_euler': free_flight,
+                                    }}
+    model.config['Modal'] = {'print_info': True,
+                            'use_undamped_modes': True,
+                            'NumLambda': num_modes,
+                            'rigid_body_modes': free_flight, 
+                            'write_modes_vtk': False,
+                            'print_matrices': False,
+                            'continuous_eigenvalues': 'off',
+                            'dt': model.dt,
+                            'plot_eigenvalues': False,
+                            }
+    model.config['SaveData']['save_linear'] = True
+    if kwargs.get('remove_gust_input_in_statespace', False):
+        model.config['LinearAssembler']['linear_system_settings']['aero_settings']['remove_inputs'] =  ['u_gust']
+
+    rom_settings = kwargs.get('rom_settings', {'use': False})
+    if rom_settings['use']:
+        model.config['LinearAssembler']['linear_system_settings']['aero_settings']['rom_method'] = rom_settings['rom_method'],
+        model.config['LinearAssembler']['linear_system_settings']['aero_settings']['rom_method_settings'] = rom_settings['rom_method_settings']
+    return model.config
---
+++ docs/source/content/example_notebooks/UDP_control/pazy_PID_controller_UDP.py
@@ -0,0 +1,133 @@
+import socket
+import logging
+import struct
+import sharpy.io.message_interface as message_interface
+import numpy as np
+from pid_controller import PID_Controller
+import json
+import os
+"""
+"""
+
+def create_and_bind_client(ip_addr, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip_addr, port))
+    return sock
+
+def setup_udp_network(dict_parameters):
+    server_ip_addr = dict_parameters["server_ip_addr"] #'127.0.0.1' # SHARPy
+    client_ip_addr =  dict_parameters["client_ip_addr"] #'127.0.0.1' # controller
+    port_in_network =  dict_parameters["port_in_network"] #64019
+    port_out_network_client = dict_parameters["port_out_network_client"] # 59009 
+    port_in_network_client = port_out_network_client - 8 # needs to be different from port out network client
+
+    sharpy_in_network= (server_ip_addr, port_in_network)
+
+    in_sock = create_and_bind_client(client_ip_addr, port_in_network_client)
+
+    out_sock = create_and_bind_client(client_ip_addr, port_out_network_client)
+    out_sock.settimeout(120)
+
+    return sharpy_in_network, in_sock, out_sock
+
+def init_logger():
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=20)
+    return logging.getLogger(__name__)
+
+def write_sensor_measurements_to_file(data, file_path, init_file=False):
+    
+    if init_file:
+        file_mode = "w"
+    else:
+        file_mode = "ab"
+
+    with open(file_path, file_mode) as f:
+        np.savetxt(f, data, delimiter=',', newline='\r\n')
+
+def receive_sensor_measurements(logger, out_sock, msg_len, num_sensors):
+    try:
+        msg, conn = out_sock.recvfrom(msg_len)
+    except socket.timeout:
+        logger.info('Socket time out')
+        raise
+    logger.info('Received {}-byte long data "{}" from {}'.format(len(msg), msg, conn))
+
+    values = message_interface.decoder(msg)
+    logger.info('Received {}'.format(values))
+    
+    results = np.zeros((num_sensors,))
+    for isensor in range(num_sensors):
+        results[isensor] = values[isensor][1]
+    return results
+
+def send_control_input(logger, in_sock, sharpy_in_network, control_input_to_send):
+    ctrl_value = struct.pack('<5sif', b'RREF0', 0, control_input_to_send)
+    logger.info('Sending control input of size {} bytes.'.format(len(ctrl_value)))
+    in_sock.sendto(ctrl_value, sharpy_in_network)
+    logger.info('Sent control input {} to {}.'.format(control_input_to_send, sharpy_in_network))
+
+def run_controller(case_name):
+
+    route_file_dir = os.path.abspath('')
+    with open(route_file_dir + '/parameter_UDP_control_{}.json'.format(case_name), 'r') as fp:
+        dict_simulation_parameters = json.load(fp)
+    
+    # define simulation settings
+    curr_ts = 0
+    num_sensors = dict_simulation_parameters["num_sensors"]
+    msg_len = 5 + 8 * num_sensors
+    dt = dict_simulation_parameters["dt"]
+    number_timesteps = int(dict_simulation_parameters["simulation_time"] / dt)
+    output_folder = dict_simulation_parameters["output_folder"]
+
+    init_cs_deflection = dict_simulation_parameters["initial_cs_deflection"]
+    reference_deflection= dict_simulation_parameters["reference_deflection"] #.020137935876846313 #0.09681699577946704
+    
+    # setup udp network components
+    sharpy_in_network, in_sock, out_sock = setup_udp_network(dict_simulation_parameters)
+
+    # setup network and data output
+    logger = init_logger()
+    file_path_sensor =  output_folder + case_name + "/{}_{}".format(case_name, "sensor_measurement")
+    file_path_control = output_folder + case_name + "./{}_{}".format(case_name, "control_input")
+
+    PID =  PID_Controller(10., 0., 0., target=reference_deflection)
+    control_input = init_cs_deflection
+    while curr_ts < number_timesteps:
+        send_control_input(logger, 
+                           in_sock, 
+                           sharpy_in_network, 
+                           control_input)
+
+        sensor_data = receive_sensor_measurements(logger, 
+                                                  out_sock, 
+                                                  msg_len,
+                                                  num_sensors)
+        
+
+        # PID controller
+        if curr_ts < 5:
+            control_input = 0
+        else:
+            control_input = PID.generate_control_input(sensor_data, dt)
+
+        
+        write_sensor_measurements_to_file(sensor_data,
+                                          file_path_sensor, 
+                                          init_file= bool(curr_ts==0))
+
+        write_sensor_measurements_to_file([control_input],
+                                          file_path_control,
+                                          init_file= bool(curr_ts==0))
+
+        curr_ts += 1
+
+    # Close sockets
+    out_sock.close()
+    in_sock.close()
+    logger.info('Closed input and output sockets')
+
+if __name__ == '__main__':
+    case_name = "pazy_udp_closed_loop_gust_response"
+    run_controller(case_name)
