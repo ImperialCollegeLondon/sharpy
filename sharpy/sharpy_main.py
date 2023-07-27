2409a1951b0545776b8d9e652e9bab5e918d1e60
Stefanie Düssler <s.dussler20@imperial.ac.uk>
2023-07-27 22:54:49 +0800

    fix use updated version of SHARPy main

--- sharpy/sharpy_main.py
+++ sharpy/sharpy_main.py
@@ -87,32 +87,18 @@ def main(args=None, sharpy_input_dict=None):
         if args.input_filename == '':
             parser.error('input_filename is a required argument of SHARPy.')
         settings = input_arg.read_settings(args)
-        missing_solvers = False
         if args.restart is None:
             # run preSHARPy
             data = PreSharpy(settings)
-            solvers = dict()
-            restart = False
         else:
             try:
                 with open(args.restart, 'rb') as restart_file:
                     data = pickle.load(restart_file)
-                    try:
-                        solvers = pickle.load(restart_file)
-                    except EOFError:
-                        # For backwards compatibility
-                        missing_solvers = True
-                        solvers = dict()
-                        cout.cout_wrap('Solvers not found in Pickle file. Using the settings in *.sharpy file.')
-                    if "UpdatePickle" in solvers.keys():
-                        # For backwards compatibility
-                        missing_solvers = True
-                        solvers = dict()
             except FileNotFoundError:
                 raise FileNotFoundError('The file specified for the snapshot \
                     restart (-r) does not exist. Please check.')
 
-            restart = True
+
             # update the settings
             data.update_settings(settings)
 
@@ -125,22 +111,11 @@ def main(args=None, sharpy_input_dict=None):
             # for it in range(self.num_steps):
             #     data.structure.dynamic_input.append(dict())
 
-            # Restart the solvers
-            old_solvers_list = list(solvers.keys())
-            for old_solver_name in old_solvers_list:
-                if old_solver_name not in settings['SHARPy']['flow']:
-                    del solvers[old_solver_name] 
-
         # Loop for the solvers specified in *.sharpy['SHARPy']['flow']
         for solver_name in settings['SHARPy']['flow']:
-            if (args.restart is None) or (solver_name not in solvers.keys()) or (missing_solvers):
-                solvers[solver_name] = solver_interface.initialise_solver(solver_name)
-            if missing_solvers:
-                solvers[solver_name].initialise(data, restart=False)
-            else:
-                solvers[solver_name].initialise(data, restart=restart)
-            data = solvers[solver_name].run(solvers=solvers)
-            solvers[solver_name].teardown()
+            solver = solver_interface.initialise_solver(solver_name)
+            solver.initialise(data)
+            data = solver.run()
 
         cpu_time = time.process_time() - t
         wall_time = time.perf_counter() - t0_wall
