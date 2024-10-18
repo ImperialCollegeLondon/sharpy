c89cd2e42d873a23cd890916b60c9b688cec8b49
kccwing <60852830+kccwing@users.noreply.github.com>
2024-10-18 11:00:10 +0530

    avoid checking for equality of a list and string for custom yb_FoR input
    ValueError: The truth value of an array with more than one element is ambiguous. Use a.any() or a.all()

--- sharpy/utils/generate_cases.py
+++ sharpy/utils/generate_cases.py
@@ -588,7 +588,10 @@ class StructuralInformation():
             y_BFoR (string): Direction of the yB axis
         """
 
-        if y_BFoR == 'x_AFoR':
+        if isinstance(y_BFoR,(list,np.ndarray)):
+            yB = np.asarray(y_BFoR)
+            cout.cout_wrap(("WARNING: custom FoR delta defined, using the given value: y_BFoR = {y_BFoR}" % (y_BFoR)), 3)
+        elif y_BFoR == 'x_AFoR':
             yB = np.array([1.0, 0.0, 0.0])
         elif y_BFoR == 'y_AFoR':
             yB = np.array([0.0, 1.0, 0.0])
