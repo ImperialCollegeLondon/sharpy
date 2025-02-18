c1633979f11e5ffd06aee4953e9d75ac03f8d2f8
Hui Ling Wong <huilingw312@gmail.com>
2025-02-18 14:15:59 +0000

    Revert back to just mayavi as the tar can't be found

--- setup.py
+++ setup.py
@@ -139,9 +139,9 @@ setup(
         "colorama",
         "dill",
         "jupyterlab",
-        #"mayavi",
-        # Fix for tvtk issue, see Issue #313
-        "mayavi @ git+https://github.com/enthought/mayavi/zipball/main",
+        "mayavi",
+        # Fix for tvtk issue, see Issue #313 - darn thing can't be found
+        #"mayavi @ git+https://github.com/enthought/mayavi/zipball/main",
         "pandas",
         "control",
         "openpyxl>=3.0.10",
